d67f2127eaabd12700f4d58d97f1ba0cdb759b70
Elaine <elainemo5510@gmail.com>
2025-10-31 12:29:16 -0400

    added untracked files

---
+++ app/(Kambaz)/Database/types.ts
@@ -0,0 +1,46 @@
+// Database/types.ts
+
+export interface Lesson {
+  _id: string;
+  name: string;
+  description: string;
+  module: string;
+}
+
+export interface Module {
+  _id: string;
+  name: string;
+  description: string;
+  course: string;
+  lessons?: Lesson[];
+}
+
+export interface Course {
+  _id: string;
+  name: string;
+  number: string;
+  startDate: string;
+  endDate: string;
+  image?: string;
+}
+
+export interface Assignment {
+  _id: string;
+  title: string;
+  course: string;
+}
+
+export interface User {
+  _id: string;
+  username: string;
+  password: string;
+  role: string;
+  firstName?: string;
+  lastName?: string;
+}
+
+export interface Enrollment {
+  _id: string;
+  user: string;
+  course: string;
+}
