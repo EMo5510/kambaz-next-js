8805646b4bae4d3e995a5f83eecbdd893fcc4444
Elaine <elainemo5510@gmail.com>
2025-10-31 12:01:02 -0400

    fixed compiling issues (mainly changing var and let into const in Lab3

--- app/(Kambaz)/Courses/[cid]/Modules/page.tsx
+++ app/(Kambaz)/Courses/[cid]/Modules/page.tsx
@@ -1,5 +1,7 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable react/jsx-key */
-"use client"
+"use client";
+
 import { useParams } from "next/navigation";
 import * as db from "../../../Database";
 import { ListGroup, ListGroupItem } from "react-bootstrap";
@@ -7,9 +9,14 @@ import ModulesControls from "./ModulesControls";
 import { BsGripVertical } from "react-icons/bs";
 import LessonControlButtons from "./LessonControlButtons";
 
+//
+import type { Module } from "../../../Database/types";
+
 export default function Modules() {
   const { cid } = useParams();
-  const modules = db.modules;
+  //const modules = db.modules;
+  const modules: Module[] = db.modules;
+
   return (
     <div>
       <ModulesControls />
@@ -21,19 +28,35 @@ export default function Modules() {
         {modules
           .filter((module: any) => module.course === cid)
           .map((module: any) => (
-          <ListGroupItem className="wd-module p-0 mb-5 fs-5 border-gray">
-            <div className="wd-title p-3 ps-2 bg-secondary">
-              <BsGripVertical className="me-2 fs-3" /> {module.name} <LessonControlButtons />
-            </div>
-            {module.lessons && (
-              <ListGroup className="wd-lessons rounded-0">
-                {module.lessons.map((lesson: any) => (
-                  <ListGroupItem className="wd-lesson p-3 ps-1">
-                    <BsGripVertical className="me-2 fs-3" /> {lesson.name} <LessonControlButtons />
-                  </ListGroupItem>
-                ))}</ListGroup>)}</ListGroupItem>))}</ListGroup>
+            <ListGroupItem
+              key={module._id || module.name}
+              className="wd-module p-0 mb-5 fs-5 border-gray"
+            >
+              <div className="wd-title p-3 ps-2 bg-secondary">
+                <BsGripVertical className="me-2 fs-3" /> {module.name}{" "}
+                <LessonControlButtons />
+              </div>
 
+              {module.lessons && (
+                <ListGroup className="wd-lessons rounded-0">
+                  {module.lessons.map((lesson: any) => (
+                    <ListGroupItem
+                      key={lesson._id || lesson.name}
+                      className="wd-lesson p-3 ps-1"
+                    >
+                      <BsGripVertical className="me-2 fs-3" /> {lesson.name}{" "}
+                      <LessonControlButtons />
+                    </ListGroupItem>
+                  ))}
+                </ListGroup>
+              )}
+            </ListGroupItem>
+          ))}
+      </ListGroup>
     </div>
+  );
+}
+
     /*<ListGroup className="rounded-0" id="wd-modules">
         
         <ListGroupItem className="wd-module p-0 mb-5 fs-5 border-gray">
@@ -96,5 +119,3 @@ export default function Modules() {
         </li>
       </ul>
     </div>*/
-  );
-}
--- app/(Kambaz)/Database/index.ts
+++ app/(Kambaz)/Database/index.ts
@@ -1,4 +1,26 @@
-import courses from "./courses.json";
-import modules from "./modules.json";
-import assignments from "./assignments.json"
-export {  courses, modules, assignments  };
+//import courses from "./courses.json";
+//import modules from "./modules.json";
+//import assignments from "./assignments.json"
+//import users from "./users.json"
+//import enrollments from "./enrollments.json"
+//export {  courses, modules, assignments, users, enrollments };
+// use types:
+import coursesData from "./courses.json";
+import modulesData from "./modules.json";
+import assignmentsData from "./assignments.json";
+import usersData from "./users.json";
+import enrollmentsData from "./enrollments.json";
+
+import type {
+  Course,
+  Module,
+  Assignment,
+  User,
+  Enrollment,
+} from "./types";
+
+export const courses: Course[] = coursesData;
+export const modules: Module[] = modulesData;
+export const assignments: Assignment[] = assignmentsData;
+export const users: User[] = usersData;
+export const enrollments: Enrollment[] = enrollmentsData;
