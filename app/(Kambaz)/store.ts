9b2644609a2bd51cc108e2a7f117838a99c61b29
Elaine <elainemo5510@gmail.com>
2025-11-03 23:35:16 -0500

    assignment reducer

--- app/(Kambaz)/store.ts
+++ app/(Kambaz)/store.ts
@@ -2,9 +2,10 @@ import { configureStore } from "@reduxjs/toolkit";
 import coursesReducer from "./Courses/reducer";
 import modulesReducer from "./Courses/[cid]/Modules/reducer";
 import accountReducer from "./Account/reducer";
+import assignmentReducer from "./Courses/[cid]/Assignments/reducer";
 
 const store = configureStore({
- reducer: { coursesReducer, modulesReducer, accountReducer },
+ reducer: { coursesReducer, modulesReducer, accountReducer, assignmentReducer },
 });
 export type RootState = ReturnType<typeof store.getState>;
 export default store;
