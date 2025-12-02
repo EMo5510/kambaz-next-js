9055268d08f9cf5069e28369c303e7cde6a76d14
Elaine <elainemo5510@gmail.com>
2025-12-01 23:12:59 -0500

    initial commit with untracked files

---
+++ app/(Kambaz)/Account/Users/page.tsx
@@ -0,0 +1,82 @@
+"use client";
+import { useState, useEffect } from "react";
+import { useParams } from "next/navigation";
+import { FormControl } from "react-bootstrap";
+import { FaPlus } from "react-icons/fa6";
+import PeopleTable from "../../Courses/[cid]/People/Table";
+import * as client from "../client";
+
+export default function Users() {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const [users, setUsers] = useState<any[]>([]);
+  const [role, setRole] = useState("");
+  const createUser = async () => {
+    const user = await client.createUser({
+      firstName: "New",
+      lastName: `User${users.length + 1}`,
+      username: `newuser${Date.now()}`,
+      password: "password123",
+      section: "S101",
+      role: "STUDENT",
+    });
+    setUsers([...users, user]);
+  };
+
+  const filterUsersByRole = async (role: string) => {
+    setRole(role);
+    if (role) {
+      const users = await client.findUsersByRole(role);
+      setUsers(users);
+    } else {
+      fetchUsers();
+    }
+  };
+  const [name, setName] = useState("");
+  const filterUsersByName = async (name: string) => {
+    setName(name);
+    if (name) {
+      const users = await client.findUsersByPartialName(name);
+      setUsers(users);
+    } else {
+      fetchUsers();
+    }
+  };
+  const { uid } = useParams();
+  const fetchUsers = async () => {
+    const users = await client.findAllUsers();
+    setUsers(users);
+  };
+  useEffect(() => {
+    fetchUsers();
+  }, [uid]);
+  return (
+    <div>
+      <button onClick={createUser} className="float-end btn btn-danger">
+        <FaPlus className="me-2" />
+        People
+      </button>
+      <h3>Users</h3>
+      <div className="d-flex mb-3 align-items-center">
+        <FormControl
+          onChange={(e) => filterUsersByName(e.target.value)}
+          placeholder="Search people"
+          className="float-start w-25 me-2 wd-filter-by-name"
+        />
+        <select
+          value={role}
+          onChange={(e) => filterUsersByRole(e.target.value)}
+          className="form-select float-start w-25 wd-select-role"
+        >
+          <option value="">All Roles</option>{" "}
+          <option value="STUDENT">Students</option>
+          <option value="TA">Assistants(TA)</option>{" "}
+          <option value="FACULTY">Faculty</option>
+          <option value="ADMIN">Administrators</option>
+        </select>
+      </div>
+      <div style={{ clear: "both" }}>
+        <PeopleTable users={users} fetchUsers={fetchUsers} />
+      </div>
+    </div>
+  );
+}
